d25a10b258d0c4b574ea434a3dd34f3f07f8cec6
moin21 <mamoinuddin@gmail.com>
2022-04-05 20:51:44 +0530

    Refactor: Logic for userRegistration validation modified to use lambda function

--- src/main/java/UserDetails.java
+++ src/main/java/UserDetails.java
@@ -1,134 +1,187 @@
-
+import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Class created for methods required for user registration
- * 
- * @author Moinuddin
+ * Functional interface -  LambdaFunction
+ * with method declaration - checkRegex
+ */
+@FunctionalInterface
+interface LambdaFunction {
+    boolean checkRegex(String regex, String UserInput);
+}
+
+/**
+ * Class created field validations for user registration
  *
+ * @author Moinuddin
  */
 public class UserDetails {
-	/**
-	 * regex - regex pattern to be matched with user input
-	 */
-	public String regex;
+    /**
+     * nameRegex - First Name and Last Name should be at least of 3 characters starting with an uppercase letter.
+     */
+    public static final String nameRegex = "^[A-Z]{1}[a-z]{2,}$";
+    /**
+     * emailRegex - allows alphanumerics with special characters in between before @ and 2-4 characters later: ex moin.asraf@mail.com
+     */
+    public static final String emailIdRegex = "^[a-zA-Z0-9]+([._+-][0-9A-Za-z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-z]{2,4})?$";
+    /**
+     * mobileNoRegex - 10 digit number followed by 2 digits country code and a space.
+     */
+    public static final String mobileNoRegex = "^[0-9]{2}\\s{1}[0-9]{10}$";
+    /**
+     * passwordRegex - Alphanumeric with at least 8 characters consisting of at least one uppercase, one number and onle one special character.
+     */
+    public static final String passwordRegex = "^[a-z](?=.*[A-Z]+)(?=.*[0-9]+)(?=.*[@$!%*#?&]{1}).{8,}$";
+    /**
+     * firstName, lastName, emailId, mobileNo and password variables for user input.
+     * nameRegex, emailIdRegex, mobileRegex, passwordRegex are the regex patterns for above specified inputs.
+     */
+    public static String firstName, lastName, emailId, mobileNo, password;
+    public static Scanner scanner = new Scanner(System.in);
 
-	/**
-	 * Method to match first name with regex
-	 * 
-	 * @param firstName - User's First Name
-	 * @return
-	 */
-	public boolean firstName(String firstName) {
-		regex = "^[A-Z]{1}[a-z]{2,}$";
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(firstName);
-		return matcher.matches();
-	}
-	/**
-	 *  In this method we will check for exception for the first name.
-	 *  If the first name is valid then it will print valid.
-	 *  If the first name is Invalid then it will throw the exception and print the message.
-	 * @param firstName - Passed first name to this method.
-	 * @return - Returns a boolean value true if correct pattern.
-	 * @throws InputInvalidException - Throws the custom exception.  
-	 */
-	public boolean testFirstName(String firstName) throws InvalidInputException {
-        try {
-            if (!firstName(firstName)) {
-                throw new InvalidInputException("Entered FirstName is Invalid\n" +
-                        "First name starts with Capital Letter and has\n" +
-                        "minimum 3 characters");
-            } else {
-                System.out.println("Entered FirstName is Valid");
-            }
-        } catch (InvalidInputException e) {
-            System.out.println("Exception is Occurred " + e);
-            e.printStackTrace();
-        }
-        return firstName(firstName);
+    /**
+     * Instance for Functional Interface.
+     * Body for checkRegex.
+     * Creating pattern object using Pattern.compile with param named regex.
+     * Creating matcher object using Pattern.matcher using param named input.
+     * returning boolean value from matcher.matches
+     */
+    static LambdaFunction validation = (regex, userInput) -> {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(userInput);
+        return matcher.matches();
+    };
+
+    /**
+     * Main method taking user inputs and calling all the validataion methods.
+     */
+    public static void main(String[] args) {
+        System.out.println("Enter First Name");
+        firstName = scanner.next();
+        firstNameValidation(firstName);
+        System.out.println("Enter Last Name");
+        lastName = scanner.next();
+        lastNameValidation(lastName);
+        System.out.println("Enter Email ID");
+        emailId = scanner.next();
+        emailValidation(emailId);
+        System.out.println("Enter Mobile No.");
+        mobileNo = scanner.next();
+        mobileNoValidation(mobileNo);
+        System.out.println("Enter Password");
+        password = scanner.next();
+        passwordValidation(password);
+        scanner.close();
     }
 
-	/**
-	 * Method to match last name with regex
-	 * 
-	 * @param lastName - User's Last Name
-	 * @return
-	 */
-	public boolean lastName(String lastName) {
-		regex = "^[A-Z]{1}[a-z]{2,}$";
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(lastName);
-		return matcher.matches();
-	}
-	/**
-	 *  In this method we will check for exception for the last name.
-	 *  If the last name is valid then it will print valid.
-	 *  If the last name is Invalid then it will throw the exception and print the message.
-	 * @param lastName - Passed last name to this method.
-	 * @return - Returns a boolean value true if correct pattern.
-	 * @throws InputInvalidException - Throws the custom exception.  
-	 */
-	public boolean testLastName(String lastName) throws InvalidInputException {
+    /**
+     * Method to validate First Name.
+     * Using try catch block to ensure NullPointerException.
+     * If validation.checkRegex gives true: Valid
+     * Else invalid.
+     *
+     * @param firstName - First name.
+     * @return - boolean value if First Name is valid or not.
+     */
+    public static boolean firstNameValidation(String firstName) {
         try {
-            if (!lastName(lastName)) {
-                throw new InvalidInputException("Entered LastName is Invalid\n" +
-                        "Last name starts with Capital Letter and has\n" +
-                        "minimum 3 characters");
-            } else {
-                System.out.println("Entered LastName is Valid");
-            }
-        } catch (InvalidInputException e) {
-            System.out.println("Exception is Occurred " + e);
-            e.printStackTrace();
+            if (validation.checkRegex(nameRegex, firstName)) {
+                System.out.println("It's a valid first name");
+                return true;
+            } else System.out.println("It's an invalid first name.");
+            return false;
+        } catch (NullPointerException nullPointerException) {
+            System.out.println("Enter a valid first name");
         }
-        return lastName(lastName);
+        return false;
     }
-	/**
-	 * Method to match email with regex
-	 * 
-	 * @param email - User's email
-	 * @return
-	 */
-	public boolean email(String email) {
-		regex = "^[a-z0-9]+([.][a-z0-9]+)*@bl[.]co[.][a-z]{2,3}$";
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(email);
-		return matcher.matches();
 
-	}
+    /**
+     * Method to validate Last Name.
+     * Using try catch block to ensure NullPointerException
+     * If validation.checkRegex gives true: Valid
+     * Else invalid.
+     *
+     * @param lastName - Last name.
+     * @return - boolean value if Last Name is valid or not.
+     */
+    public static boolean lastNameValidation(String lastName) {
+        try {
+            if (validation.checkRegex(nameRegex, lastName)) {
+                System.out.println("It's a valid last name");
+                return true;
+            } else System.out.println("It's an invalid last name.");
+            return false;
+        } catch (NullPointerException nullPointerException) {
+            System.out.println("Enter a valid last name");
+        }
+        return false;
+    }
 
-	/**
-	 * Method to match phone with regex
-	 * 
-	 * @param mobile - User's mobile
-	 * @return
-	 */
-	public boolean mobile(String mobile) {
-		regex = "^[0-9]{2}(\\s){1}[0-9]{10}$";
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(mobile);
-		return matcher.matches();
-	}
+    /**
+     * Method to validate Email.
+     * Using try catch block to ensure NullPointerException
+     * If validation.checkRegex gives true: Valid
+     * Else invalid.
+     *
+     * @param emailId - Email ID.
+     * @return - boolean value if Email is valid or not.
+     */
+    public static boolean emailValidation(String emailId) {
+        try {
+            if (validation.checkRegex(emailIdRegex, emailId)) {
+                System.out.println("It's a valid email id");
+                return true;
+            } else System.out.println("It's an invalid email id.");
+            return false;
+        } catch (NullPointerException nullPointerException) {
+            System.out.println("Enter a valid email id");
+        }
+        return false;
+    }
 
-	/***
-	 * Method to match password with regex
-	 * 
-	 * @param password - User's password
-	 * @return
-	 */
-	public boolean password(String password) {
-		regex = "^[a-zA-Z0-9]{8,}$";
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(password);
-		return matcher.matches();
-	}
+    /**
+     * Method to validate Mobile Number.
+     * Using try catch block to ensure NullPointerException
+     * If validation.checkRegex gives true: Valid
+     * Else invalid.
+     *
+     * @param mobileNo - Mobile Number.
+     * @return - boolean value if Mobile Number is valid or not.
+     */
+    public static boolean mobileNoValidation(String mobileNo) {
+        try {
+            if (validation.checkRegex(mobileNoRegex, mobileNo)) {
+                System.out.println("It's a valid phone no.");
+                return true;
+            } else System.out.println("It's an invalid phone no.");
+            return false;
+        } catch (NullPointerException nullPointerException) {
+            System.out.println("Enter a valid phone no.");
+        }
+        return false;
+    }
 
-	public boolean upperCase(String upperCase) {
-		regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*#?&]{1})[A-Za-z\\d@$!%*?&]{8,}$";
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(upperCase);
-		return matcher.matches();
-	}
+    /**
+     * Method to validate Password.
+     * Using try catch block to ensure NullPointerException
+     * If validation.checkRegex gives true: Valid
+     * Else invalid.
+     *
+     * @param password - Password.
+     * @return - boolean value if Password is valid or not.
+     */
+    public static boolean passwordValidation(String password) {
+        try {
+            if (validation.checkRegex(passwordRegex, password)) {
+                System.out.println("It's a valid password.");
+                return true;
+            } else System.out.println("It's an invalid password.");
+            return false;
+        } catch (NullPointerException nullPointerException) {
+            System.out.println("Enter a valid password.");
+        }
+        return false;
+    }
 }
--- src/test/java/RegexTest.java
+++ src/test/java/RegexTest.java
@@ -1,61 +1,70 @@
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Junit Test class to verify UserDetail's Methods
- * 
- * @author Moinuddin
  *
+ * @author Moinuddin
  */
 public class RegexTest {
-	/**
-	 * New instance userDetails created for UserDetails
-	 */
-	UserDetails userDetails = new UserDetails();
 
-	/**
-	 * Method to verify first name
-	 */
-	@Test
-	public void testFistName() {
-		boolean result = userDetails.firstName(null);
-		assertTrue(result);
-	}
 
-	/**
-	 * Method to verify last name
-	 */
-	@Test
-	public void testLastName() {
-		boolean result = userDetails.lastName("Asraf");
-		assertTrue(result);
-	}
+    /**
+     * Method to test first name validation method when Proper first name is given.
+     */
 
-	/**
-	 * Method to verify email
-	 */
-	@Test
-	public void testEmail() {
-		boolean result = userDetails.email("moinudin.asraf@bl.co.in");
-		assertTrue(result);
-	}
+    @Test
+    public void givenProperFirstName_returnTrue() {
 
-	/**
-	 * Method to test Mobile no.
-	 */
-	@Test
-	public void testMobile() {
-		boolean result = userDetails.mobile("91 9602721596");
-		assertTrue(result);
-	}
+        UserDetails userDetails = new UserDetails();
+        assertTrue(userDetails.firstNameValidation("Alpha"));
+    }
 
-	/**
-	 * Method to test password
-	 */
-	@Test
-	public void testPassword() {
-		boolean result = userDetails.password("moiN12345");
-		assertTrue(result);
-	}
+    /**
+     * Method to test first name validation method when Improper first name is given.
+     */
+    @Test
+    public void givenImproperFirstName_returnFalse() {
+
+        UserDetails userDetails = new UserDetails();
+        assertFalse(userDetails.firstNameValidation("alpha"));
+    }
+    /**
+     * Method to test first name validation method when first name is null.
+     */
+    @Test
+    public void givenNullFirstName_returnFalseByNullPointerException() {
+
+        UserDetails userDetails = new UserDetails();
+        assertFalse(userDetails.firstNameValidation(null));
+    }
+    /**
+     * Method to test last name validation method when Proper last name is given.
+     */
+    @Test
+    public void givenProperLastName_returnTrue() {
+
+        UserDetails userDetails = new UserDetails();
+        assertTrue(userDetails.lastNameValidation("Alpha"));
+    }
+    /**
+     * Method to test last name validation method when Improper last name is given.
+     */
+    @Test
+    public void givenImproperLastName_returnFalse() {
+
+        UserDetails userDetails = new UserDetails();
+        assertFalse(userDetails.lastNameValidation("alpha"));
+    }
+    /**
+     * Method to test last name validation method when last name is given null.
+     */
+    @Test
+    public void givenNullLastName_returnFalseByNullPointerException() {
+
+        UserDetails userDetails = new UserDetails();
+        assertFalse(userDetails.lastNameValidation(null));
+    }
 }
